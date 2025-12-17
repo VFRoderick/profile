af34dadd47bc39d44621df77fd397b9c9e48b48b
Roderick Perendy <roderick.perendy@gmail.com>
2025-12-17 08:56:41 -0500

    added diagram.lua script

--- pandoc/diagram.lua
+++ pandoc/diagram.lua
@@ -1,1 +1,8 @@
-_extensions/diagram/diagram.lua
+-- diagram.lua
+-- Converts mermaid code blocks to raw HTML that Mermaid.js can render client-side
+
+function CodeBlock(block)
+  if block.classes[1] == "mermaid" then
+    return pandoc.RawBlock("html", '<pre class="mermaid">\n' .. block.text .. '\n</pre>')
+  end
+end
